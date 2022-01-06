0c52fc6f30b793d832d34eab455d3171d5da3379
Joe Zucco <josephzucco@gmail.com>
2022-01-06 11:02:23 -0500

    set up boilerplate Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 const Header = () => {
-  return <div></div>
+  return (
+    <header>
+      <div className='container'>
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  )
 }
 
 export default Header
