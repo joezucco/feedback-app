06abf6e792a274726db8a4f2b3201615decc49ad
Joe Zucco <josephzucco@gmail.com>
2022-04-14 16:08:33 -0400

    import feedback data as separate file

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,11 +4,6 @@ export const FeedbackItem = () => {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of a feedback item');
 
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1
-    });
-  };
   return (
     <div className='card'>
       <div className='num-display'>{rating}</div>
