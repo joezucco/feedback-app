db398e4c08a5d963e399df969d5d72b50ef9b372
Joe Zucco <josephzucco@gmail.com>
2022-04-14 15:48:03 -0400

    play with css passing as props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/Header'
+import Header from './components/Header';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <h1>My App</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
