cfc1ea779b142abb4ebeba858d7667af4888ea26
Joe Zucco <josephzucco@gmail.com>
2022-04-14 16:01:35 -0400

    component level state intro

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const FeedbackItem = () => {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState('This is an example of a feedback item');
+
+  const handleClick = () => {
+    setRating((prev) => {
+      return prev + 1
+    });
+  };
+  return (
+    <div className='card'>
+      <div className='num-display'>{rating}</div>
+      <div className='text-display'>{text}</div>
+    </div>
+  );
+};
+
+export default FeedbackItem;
